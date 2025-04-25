88fc7527af404e460a98933dc3df16643a6a2ea2
Mo Mobarak <mo@mobarak.com>
2025-04-25 18:45:11 +0100

    working motor action

--- src/actions/MotorAction.cpp
+++ src/actions/MotorAction.cpp
@@ -4,41 +4,19 @@
 
 #include "MotorAction.h"
 
-const unsigned long pauseDuration = 500; // ms pause between movements
-
 void MotorAction::performAction(uint32_t actionDurationMillis)
 {
-    TickType_t startTick = xTaskGetTickCount();
-    // Convert duration directly from milliseconds to ticks
     TickType_t durationTicks = pdMS_TO_TICKS(actionDurationMillis);
+    TickType_t startTick = xTaskGetTickCount();
     TickType_t endTick = startTick + durationTicks;
 
-    const TickType_t delayIntervalTicks = pdMS_TO_TICKS(actionDurationMillis);
-
     Serial.println("Performing MotorAction for " + String(actionDurationMillis) + " ms.");
-
+    
     Motor::Direction direction = Motor::Direction::FORWARD;
     while (xTaskGetTickCount() < endTick)
     {
-        TickType_t currentTime = xTaskGetTickCount();
-        TickType_t remainingTime = (endTick > currentTime) ? (endTick - currentTime) : 0; // Prevent underflow
-
         _motor.moveFullRangeInDirection(direction);
         direction = direction == Motor::Direction::FORWARD ? Motor::Direction::REVERSE : Motor::Direction::FORWARD;
-
-        if (remainingTime == 0)
-        {
-            break; // Exit if duration already passed
-        }
-        else if (remainingTime < delayIntervalTicks)
-        {
-            vTaskDelay(remainingTime); // Delay for the exact remaining time
-            break;                     // Exit after final delay
-        }
-        else
-        {
-            vTaskDelay(delayIntervalTicks); // Delay for the standard interval
-        }
     }
 
     Serial.println("Finished performing MotorAction.");
--- src/main.cpp
+++ src/main.cpp
@@ -16,36 +16,17 @@ void setup()
 void loop()
 {
   Serial.println("Creating PeriodicActions...");
-  //PeriodicAction<NeoPixelAction> periodicNeoPixelAction(10000UL, 3 /* UINT32_MAX */, 3000UL, NeoPixel::StateColor::OK);
-  PeriodicAction<MotorAction> periodicMotorAction(16000UL, 3, 5000UL);
+  PeriodicAction<NeoPixelAction> periodicNeoPixelAction(10000UL, 30 /* UINT32_MAX */, 3000UL, NeoPixel::StateColor::OK);
+  PeriodicAction<MotorAction> periodicMotorAction(60000UL, 3, 10000UL);
   vTaskDelay(pdMS_TO_TICKS(2000));
 
 
   Serial.println("Starting PeriodicActions...");
-  //periodicNeoPixelAction.start();
+  periodicNeoPixelAction.start();
   periodicMotorAction.start();
   Serial.println("PeriodicActions started.");
 
   while(true) {
-    // Serial.println("Just waiting around for 2 seconds...");
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
-
-
-  // Motor motor;
-
-  // while (true)
-  // {
-  //   Serial.println("Moving FORWARD");
-  //   motor.moveFullRangeInDirection(Motor::Direction::FORWARD);
-
-  //   // Serial.println("Waiting 1 seconds");
-  //   // vTaskDelay(pdMS_TO_TICKS(500));
-
-  //   Serial.println("Moving REVERSE");
-  //   motor.moveFullRangeInDirection(Motor::Direction::REVERSE);
-
-  //   Serial.println("Waiting 2 seconds");
-  //   vTaskDelay(pdMS_TO_TICKS(2000));
-  // }
 }
