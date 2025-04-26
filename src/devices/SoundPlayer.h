1e87e2ca28a2e6167026871ed72c2a4be21eb9b1
Mo Mobarak <mo@mobarak.com>
2025-04-26 17:17:55 +0100

    wip

--- src/devices/SoundPlayer.h
+++ src/devices/SoundPlayer.h
@@ -11,31 +11,45 @@ class SoundPlayer
 public:
     enum Sound
     {
+        TEST_TONE = 0,
         SPECIES_1,
         SPECIES_2,
-        TEST_TONE
+        NUM_SOUNDS
     };
 
     void playSound(Sound sound)
     {
-        String soundFile = _soundFiles[sound];
+        int soundFileNum = static_cast<int>(sound) + 1;
+        if (soundFileNum < 0 || soundFileNum >= NUM_SOUNDS)
+        {
+            Serial.println("Error: Invalid sound enum value.");
+            return;
+        }
 
-        if (!_soundPlayer.isPlaying()) {
-           Serial.println("Starting to play " + soundFile);
-            _soundPlayer.playSpecFile(soundFile);        
-        } else {
-            Serial.println("Skipping " + soundFile + ".  A sound file is currently playing.");          
+        if (!_soundPlayer.isPlaying())
+        {
+            Serial.println("Starting to play sound file number " + String(soundFileNum));
+            _soundPlayer.playFileNum(2);
+
+            while (_soundPlayer.isPlaying())
+                delay(500);
+
+            // Add some voodoo to make it play something other than the first file
+            _soundPlayer.pause();
+            _soundPlayer.next();
+            _soundPlayer.pause();
+        }
+        else
+        {
+            Serial.println("Skipping sound file number " + String(soundFileNum) + ".  A sound file is currently playing.");
         }
     }
 
-
-    SoundPlayer(int rxPin = defaultRxPin, int txPin = defaultTxPin):
-    _serial(rxPin, txPin), // Initialize the SoftwareSerial member
-    _soundPlayer()         // Initialize the DFRobot member
+    SoundPlayer(int rxPin = defaultRxPin, int txPin = defaultTxPin) : _serial(rxPin, txPin) // Initialize the SoftwareSerial member
     {
 
         _serial.begin(115200);
-        while(!Serial)
+        while (!_serial)
         {
             Serial.println("Waiting for serial");
             delay(500);
@@ -46,15 +60,13 @@ public:
             delay(1000); // Retry if initialization fails
         }
 
-        _soundPlayer.setVol(/*30*/ 5);                    // MOFIX Set volume 0-30 - set to max for deployment
+        _soundPlayer.setVol(/*30*/ 20);                  // MOFIX Set volume 0-30 - set to max for deployment
         _soundPlayer.switchFunction(_soundPlayer.MUSIC); // Set MP3 module mode to music
         _soundPlayer.setPlayMode(_soundPlayer.SINGLE);   // Set single track play mode
-        _soundPlayer.setPrompt(false);                    // Disable prompt sounds
+        _soundPlayer.setPrompt(false);                   // Disable prompt sounds
     };
 
 private:
-SoftwareSerial _serial;
-DFRobot_DF1201S _soundPlayer;
-    String _soundFiles[3] = {"species-1.mp3", "species-2.mp3", "test-tone.mp3"};
-
+    SoftwareSerial _serial;
+    DFRobot_DF1201S _soundPlayer;
 };
